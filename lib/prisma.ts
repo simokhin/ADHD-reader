9a78af5c7154abba21740534c6bcb31adb50b9ef
Nikita <nick.simokhin@yandex.ru>
2025-03-28 03:29:10 +0300

    Исправил ошибку с типом данных

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
