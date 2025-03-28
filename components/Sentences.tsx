2e9d95ca32bc78ebd17a288b3a89a424ff7b4d62
Nikita <nick.simokhin@yandex.ru>
2025-03-28 05:09:42 +0300

    Временно закомментил код с бд и аутентификацией

--- components/Sentences.tsx
+++ components/Sentences.tsx
@@ -1,3 +1,6 @@
+import { Bookmark } from "lucide-react";
+import { Button } from "./ui/button";
+
 interface SentencesProps {
   displayedSentences: string[];
   lastParagraphRef: React.RefObject<HTMLParagraphElement | null>;
@@ -13,19 +16,25 @@ export default function Sentences({
       style={{ maxHeight: "300px", minHeight: "300px", overflowY: "auto" }}
     >
       {displayedSentences.map((sentence, index) => (
-        <p
+        <div
           key={index}
           ref={
             index === displayedSentences.length - 1 ? lastParagraphRef : null
           }
-          className={`mb-2 px-2 py-2 transition-all ease-in-out ${
+          className={`mb-2 px-2 py-2 transition-all ease-in-out flex justify-between items-center ${
             index === displayedSentences.length - 1
               ? "bg-chart-4 rounded-lg"
               : ""
           }`}
         >
-          {sentence}
-        </p>
+          <p>{sentence}</p>
+          <Button
+            variant="ghost"
+            className="opacity-30 hover:cursor-pointer hover:opacity-100"
+          >
+            <Bookmark />
+          </Button>
+        </div>
       ))}
     </div>
   );
